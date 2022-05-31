a86c8361da969026ddd00012187bf7d9141b3be6
zourong <zourong@126.com>
2022-05-31 23:04:06 +0800

    feat: 加载逻辑添加

--- src/views/pages/[id].vue
+++ src/views/pages/[id].vue
@@ -4,54 +4,39 @@
 
 <script lang="ts">
 import { defineComponent, getCurrentInstance, reactive, onMounted } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { useRoute } from "vue-router";
 import { getPageFrameDataXhr } from "~/service/index";
 
 export default defineComponent({
   setup() {
     const { $toast, $loading } = getCurrentInstance()?.proxy;
     const $route = useRoute();
-    const $router = useRouter();
     const state = reactive({
       pageId: $route.params?.id,
       pageData: {},
     });
 
     /**
-     * 页面初始数据的回调处理
-     */
-    const cbLoadPageData = (data: any) => {
-      return data;
-    };
-
-    /**
      * 加载页面初始数据
      */
     const onLoadPageData = async () => {
-      const loading = $loading();
-      try {
-        const res = await getPageFrameDataXhr({ id: state.pageId });
-        state.pageData = cbLoadPageData(res.data);
-      } catch (e) {}
-      loading.close();
-    };
-
-    /**
-     * 初始化页面
-     */
-    const onInitPage = () => {
       if (!state.pageId) {
         $toast.error("当前页面id无效，请检查页面链接是否正确！");
         return;
       }
-      onLoadPageData();
+      const loading = $loading();
+      try {
+        const res = await getPageFrameDataXhr({ id: state.pageId });
+        state.pageData = res.data;
+      } catch (e) {}
+      loading.close();
     };
 
     onMounted(() => {
-      onInitPage();
+      onLoadPageData();
     });
 
-    return { state, cbLoadPageData, onLoadPageData, onInitPage };
+    return { state, onLoadPageData };
   },
 });
 </script>
