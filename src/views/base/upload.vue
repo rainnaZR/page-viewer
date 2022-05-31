088016522855c5a23814bd9844f92a7b9855fe93
zourong <zourong@126.com>
2022-06-01 00:13:04 +0800

    feat: 页面上传

---
+++ src/views/base/upload.vue
@@ -0,0 +1,29 @@
+<template>
+  <ht-upload v-model:modelValue="files" :data="uploadData" />
+  <div v-for="(item, index) in files" :key="index">
+    <a :href="`/pages/${item.match(/\d+/g)[0]}`" target="_blank">
+      页面{{ index + 1 }}
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from "vue";
+
+export default defineComponent({
+  setup() {
+    const files = ref([]);
+    const uploadData = reactive({
+      multiple: true,
+      extensions: "json",
+      action: "https://production.api.dahuangf.com/api/base/getQiNiuToken",
+      imgDomain: "https://image.dahuangf.com",
+    });
+
+    return {
+      files,
+      uploadData,
+    };
+  },
+});
+</script>
