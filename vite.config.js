3015f06e3d492b967e69c07e1b4081cfe17f86c7
rainnaZR <denzu111@163.com>
2022-06-01 09:58:30 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import pages from "vite-plugin-pages";
+import viteCompression from "vite-plugin-compression";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    pages({
+      dirs: [{ dir: "src/views", baseRoute: "" }],
+    }),
+    viteCompression({
+      verbose: false,
+      disable: false,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+  resolve: {
+    // 路径别名
+    alias: {
+      "@": resolve(__dirname, "./src/components"),
+      "~": resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    include: [
+      "axios",
+      "@htfed/ui",
+      "@htfed/utils",
+      "vue",
+      "vue-router",
+      "vxe-table",
+    ],
+  },
+});
