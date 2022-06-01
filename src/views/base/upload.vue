cd2abbd71b308c3cbe4b38076e72e17cfca494f6
zourong <zourong@126.com>
2022-06-01 11:18:46 +0800

    feat: 上传页面修改

--- src/views/base/upload.vue
+++ src/views/base/upload.vue
@@ -1,29 +1,45 @@
 <template>
-  <ht-upload v-model:modelValue="files" :data="uploadData" />
-  <div v-for="(item, index) in files" :key="index">
-    <a :href="`/pages/${item.match(/\d+/g)[0]}`" target="_blank">
-      页面{{ index + 1 }}
-    </a>
+  <div class="m-box">
+    <h2 class="f-mb20 f-fw1 f-fs20">页面上传</h2>
+    <ht-upload
+      v-model:modelValue="files"
+      :data="{
+        multiple: true,
+        extensions: 'json',
+        action: 'https://production.api.dahuangf.com/api/base/getQiNiuToken',
+        imgDomain: 'https://image.dahuangf.com',
+      }"
+    />
+
+    <div v-for="(item, index) in files" :key="index">
+      <a :href="item" target="_blank">{{ item }}</a>
+      <a
+        :href="`/pages/${item.match(/\d+/g)[0]}`"
+        target="_blank"
+        class="f-ml20"
+        >预览页面</a
+      >
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
     const files = ref([]);
-    const uploadData = reactive({
-      multiple: true,
-      extensions: "json",
-      action: "https://production.api.dahuangf.com/api/base/getQiNiuToken",
-      imgDomain: "https://image.dahuangf.com",
-    });
 
     return {
       files,
-      uploadData,
     };
   },
 });
 </script>
+
+<style lang="less" scoped>
+.m-box {
+  width: 800px;
+  margin: 20px auto;
+}
+</style>
